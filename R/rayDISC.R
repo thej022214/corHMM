7d62467b72dc0c7e191b4f3326e85caaf2a755b7
thej022214 <jbeaulieu@nimbios.org>
2018-05-23 23:41:05 -0500

    trying to debug new code

--- R/rayDISC.R
+++ R/rayDISC.R
@@ -425,8 +425,6 @@ dev.raydisc <- function(p, phy, liks, Q, rate, root.p, lewis.asc.bias){
             liks[focal, ] <- v/comp[focal]
         }
     }
-    print(liks)
-    print(comp)
 	#Specifies the root:
 	root <- nb.tip + 1L
 	#If any of the logs have NAs restart search:
