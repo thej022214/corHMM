3f26fb7645c9cce8cdd516bd6a8b0db501a1fce0
Jeremy Beaulieu <jmbeauli@uaf134474.ddns.uark.edu>
2019-01-08 09:05:13 -0600

    testing

--- R/ancRECON.R
+++ R/ancRECON.R
@@ -536,7 +536,7 @@ ancRECON <- function(phy, data, p, method=c("joint", "marginal", "scaled"), hrm=
 				}else{
                     #If the mother is the root then just use the marginal. This can also be the prior, which I think is the equilibrium frequency.
                     #But for now we are just going to use the marginal at the root -- it is unclear what Mesquite does.
-					v <- liks.down[root, ]
+					v <- 1
 				}
 				#Now calculate the probability that each sister is in either state. Sister can be more than 1 when the node is a polytomy.
 				#This is essentially calculating the product of the mothers probability and the sisters probability:
@@ -559,7 +559,7 @@ ancRECON <- function(phy, data, p, method=c("joint", "marginal", "scaled"), hrm=
 			#everything at and above focal by the probability of the mother and all the sisters given time t:
 			v <- liks.down[focal,]*expm(tranQ * phy$edge.length[focalRows], method=c("Ward77")) %*% liks.up[focal,]
 			comp[focal] <- sum(v)
-			liks.final[focal, ] <- v/comp[focal]
+			liks.final[focal, ] <- (v/comp[focal]) * root.p
 		}
 
         if(get.tip.states == TRUE){
