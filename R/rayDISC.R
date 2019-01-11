d13c268f6b0cf62d0d0cd007943afc72f45187af
thej022214 <jbeaulieu@nimbios.org>
2019-01-10 22:00:51 -0600

    testing

--- R/rayDISC.R
+++ R/rayDISC.R
@@ -419,7 +419,7 @@ dev.raydisc <- function(p, phy, liks, Q, rate, root.p, lewis.asc.bias){
             v <- v * expm(Q * phy$edge.length[desRows[desIndex]], method=c("Ward77")) %*% liks[desNodes[desIndex],]
         }
         comp[focal] <- sum(v)
-        if(anc == (Ntip(phy)+1)){
+        if(anc[i] == (Ntip(phy)+1)){
             print(liks[focal, ])
             print(liks[focal, ] /sum(liks[focal, ]))
         }
